6ce85e7110645a4a9797d3d5026761f14b80517e
Jihye Gim <77537852+GimWise@users.noreply.github.com>
2021-07-25 22:23:29 +0900

    home

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('common/', include('common.urls'),)
+    path('common/', include('common.urls')),
+    path('home/', include('home.urls')),
 ]
---
+++ home/static/js/home.js
@@ -0,0 +1,10 @@
+// Home - fullPage scroll
+$(function(){
+    $('#fullpage').fullpage({
+        anchors:['section1', 'section2', 'section3', 'section4'],
+        navigation:true,
+        navigationPosition:'right',
+    });
+});
+
+
