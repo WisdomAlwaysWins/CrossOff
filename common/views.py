463434b68b1ce19304c630894cbbd5f8a9876a28
Hyunjik Lee <jiggy0429@gmail.com>
2021-08-11 16:28:49 +0900

    error

--- common/views.py
+++ common/views.py
@@ -1,63 +1,28 @@
 from django.contrib.auth.forms import PasswordResetForm
+from django.http import response
 from django.shortcuts import redirect, render, resolve_url
+from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import (
-    AuthenticationForm,
-    PasswordChangeForm,
-    PasswordResetForm,
-    SetPasswordForm,
-)
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetView, PasswordResetDoneView
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetView, PasswordResetDoneView
 from .forms import *
 from .models import *
 from mandalart.models import *
 from django.conf import settings
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import auth
-from django.contrib import messages
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.shortcuts import resolve_url
-from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
-from django.views.generic.edit import CreateView
-from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
-from django.views import generic, View
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.contrib.auth import login, authenticate
-from django.template import RequestContext
-from django.http import HttpResponse
 try:
     from django.utils import simplejson as json
 except ImportError:
     import json
-from django.http import HttpResponse, HttpResponseRedirect
-from django.views.decorators.http import require_POST
-from django.contrib.auth import (
-    REDIRECT_FIELD_NAME,
-    get_user_model,
-    login as auth_login,
-    logout as auth_logout,
-    update_session_auth_hash,
-)
-from django.contrib.auth.forms import (
-    AuthenticationForm,
-    PasswordChangeForm,
-    PasswordResetForm,
-    SetPasswordForm,
-)
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.decorators import method_decorator
-from django.utils.http import is_safe_url, urlsafe_base64_decode
-from django.views.decorators.debug import sensitive_post_parameters
-from django.views.decorators.cache import never_cache
 from django.utils.translation import gettext_lazy as _
-
+from django.views import generic
 User = get_user_model()
 INTERNAL_RESET_URL_TOKEN = 'set-password'
 INTERNAL_RESET_SESSION_TOKEN = '_password_reset_token'
@@ -198,22 +163,6 @@ def profile(request, id):
     return render(request, 'common/seeProfile.html', {'user': user})
 
 
-# def password_reset(request):
-#     return render(request, 'common/password_reset.html')
-
-# def password_reset_done(request):
-#     return render(request, 'common/password_reset_done.html')
-
-# def password_reset_done_fail(request):
-#     return render(request, 'common/password_reset_done_fail.html')
-
-# def password_reset_confirm(request):
-#     return render(request, 'common/password_reset_confirm.html')
-
-# def password_reset_complete(request):
-#     return render(request, 'common/password_reset_complete.html')
-
-
 @login_required
 def profileUpdate(request):
     if request.method == 'POST':
@@ -242,27 +191,6 @@ def passwordEdit(request):
         password_change_form = CustomPasswordChangeForm(request.user)
     return render(request, 'common/editPassword.html',
                   {'password_change_form': password_change_form})
-
-
-# class UserPasswordResetView(auth_views.PasswordResetView):
-#     template_name = 'password_reset_form.html' #템플릿을 변경하려면 이와같은 형식으로 입력
-
-#     def form_valid(self, form):
-#         if User.objects.filter(email=self.request.POST.get("email")).exists():
-#             opts = {
-#                 'use_https': self.request.is_secure(),
-#                 'token_generator': self.token_generator,
-#                 'from_email': self.from_email,
-#                 'email_template_name': self.email_template_name,
-#                 'subject_template_name': self.subject_template_name,
-#                 'request': self.request,
-#                 'html_email_template_name': self.html_email_template_name,
-#                 'extra_email_context': self.extra_email_context,
-#             }
-#             form.save(**opts)
-#             return super().form_valid(form)
-#         else:
-#             return render(self.request, 'password_reset_done_fail.html')
 
 
 class UserPasswordResetView(PasswordResetView):
@@ -299,3 +227,21 @@ class UserPasswordResetCompleteView(PasswordResetCompleteView):
         context = super().get_context_data(**kwargs)
         context['login_url'] = resolve_url(settings.LOGIN_URL)
         return context
+
+
+def custom_500_error(request):
+    response = render(request, 'errors/500.html')
+    response.status_code = 500
+    return response
+
+
+def custom_404_error(request, exception):
+    response = render(request, 'errors/404.html')
+    response.status_code = 404
+    return response
+
+
+def custom_400_error(request, exception):
+    response = render(request, "errors/400.html")
+    response.status_code = 400
+    return response
--- config/urls.py
+++ config/urls.py
@@ -13,25 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler400
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 import common.views as cv
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('common/', include('common.urls')),
     path('', include('home.urls')),
     path('mandalart/', include('mandalart.urls')),
-
-    # path('password_reset/', cv.password_reset, name="password_reset"),
-    # path('password_reset_done/', cv.password_reset_done, name="password_reset_done"),
-    # path('password_reset_done_fail/', cv.password_reset_done_fail, name="password_reset_done_fial"),
-    # path('password_reset_confirm/', cv.password_reset_confirm, name="password_reset_confirm"),
-    # path('password_reset_complete/', cv.password_reset_complete, name="password_reset_complete"),
-
     path('password_reset/', cv.UserPasswordResetView.as_view(), name="password_reset"),
     path('password_reset_done/', cv.UserPasswordResetDoneView.as_view(), name="password_reset_done"),
     path('password_reset_confirm/<uidb64>/<token>/', cv.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('password_reset_complete/', cv.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
+
+handler500 = 'common.views.custom_500_error'
+handler404 = 'common.views.custom_404_error'
+handler400 = 'common.views.custom_400_error'
